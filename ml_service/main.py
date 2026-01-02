fa0060511b388972c9787ebe7541de46432508f3
Kamal-san <kamaleshw046@gmail.com>
2026-01-02 15:49:43 +0530

    fix  CORS for vercel app

--- ml_service/main.py
+++ ml_service/main.py
@@ -43,6 +43,7 @@ app.add_middleware(
     allow_origins=["http://localhost:5173",
                    "https://loan-approval-ml-eight.vercel.app",
                    "https://loan-approval-7hcq23vxm-kamaleshwarans-projects-3187a12c.vercel.app"],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
