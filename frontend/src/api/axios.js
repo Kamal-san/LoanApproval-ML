91c73d7acddd1813f15637aa6f978b6fe1596f64
Kamal-san <kamaleshw046@gmail.com>
2026-01-02 15:52:41 +0530

    Modified axios- baseurl

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "https://loanapproval-ml.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
