a3cd0425c6271c1dc02bbe73d0641923dfdb7ad7
Kamal-san <kamaleshw046@gmail.com>
2026-01-02 17:15:01 +0530

    enable CORS for vercel

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://loanapproval-ml.onrender.com/docs",
+  baseURL: "https://loanapproval-ml.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
